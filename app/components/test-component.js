15fc7eccd904e3515714ed4339d7a93ad6280c52
Gab-Metzger <gab.metzger@gmail.com>
2015-07-27 12:31:14 +0200

    One possible solution to the issue

--- app/components/test-component.js
+++ app/components/test-component.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  exampleValue: window.EmberEnvVarsProd.EXAMPLE_VAR
+  exampleValue: window.EmberENV.EXAMPLE_VAR
 });
--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,7 @@ module.exports = function(environment) {
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
+      EXAMPLE_VAR: process.env.EXAMPLE_VAR,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
@@ -16,7 +17,6 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-      EXAMPLE_VAR: process.env.EXAMPLE_VAR
     }
   };
 
